688d163ec159be570ffc3d9a43f96945951a19b1
Xiangyu Wang <xiangyu.laura@icloud.com>
2018-07-20 12:10:36 +0800

    version_0.0.16

--- auto.py
+++ auto.py
@@ -1,46 +1,18 @@
 import os, sys
 import fileinput
 
-sources = [
-          #'https://github.com/laurawxy/XYUIFramework.git',
-          ]
-
 project_name = 'XYUIFramework'
 podspec_file_name = 'XYUIFramework.podspec'
 
 
 new_tag = ""
-lib_command = ""
-pod_push_command = ""
+lib_command = "pod lib lint --allow-warnings"
+#pod_push_command = "pod trunk push --allow-warnings"
+
+pod_push_command = 'pod repo push ' + project_name + podspec_file_name + '--allow-warnings'
+
 spec_file_path = "./" + podspec_file_name
 find_version_flag = False
-
-
-
-
-def podCommandEdit():
-    global lib_command
-    global pod_push_command
-    source_suffix = 'https://github.com/CocoaPods/Specs.git --allow-warnings'
-    lib_command = 'pod lib lint --sources= --allow-warnings'
-#    pod_push_command = 'pod repo push ' + project_name + ' ' + podspec_file_name + ' --allow-warnings'
-    pod_push_command = 'pod trunk push --allow-warnings'
-
-    if len(sources) > 0:
-        # rely on  private sourece
-        pod_push_command += ' --sources='
-
-        for index,source in enumerate(sources):
-            lib_command += source
-            lib_command += ','
-            pod_push_command += source
-            pod_push_command += ','
-
-        lib_command += source_suffix
-        pod_push_command += source_suffix
-
-    else:
-        lib_command = 'pod lib lint --allow-warnings'
 
 
 def updateVersion():
@@ -110,6 +82,8 @@ def libLint():
     os.system(lib_command)
 
 def gitOperation():
+    print "--------- waiting update git -------- "
+
     os.system('git add .')
     commit_desc = "version_" + new_tag
     commit_command = 'git commit -m "' + commit_desc + '"'
@@ -135,7 +109,6 @@ def podPush():
 
 
 updateVersion()
-podCommandEdit()
 libLint()
 gitOperation()
 podPush()
